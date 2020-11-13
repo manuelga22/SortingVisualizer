5c619d3f32328f034368fe68cf3776d674e1ebba
Digital Communications <digitalcommunications@OMMBP.local>
2020-11-12 23:51:09 -0500

    generating bars

--- src/Sorting/SortingVisualizer.js
+++ src/Sorting/SortingVisualizer.js
@@ -1,26 +1,41 @@
-import { useEffect,Fragment } from 'react';
+import { useEffect,useState, Fragment} from 'react';
 import {quickSort} from './SortingAlgorithms';
 import './SortingVisualizer.css'
 function SortingViasualizer() {
-    
-    
+    const [bars, setBars] = useState([]);
+
+    let barsHeights=[];
      
-    function generateBars(number){
-        
-        let bars = `` ;
+    function generateBars(number){ 
+        let barsArray=[];     
+        let bar = ``
         let counter = 0;
         while (counter < number){
-            bars= document.createElement('div')
-            bars.classList = "bar"
-            document.getElementById("Visualizer").appendChild(bars)
-            counter++;
-        } 
 
+            let random_num = Math.floor((Math.random() * 50) + 1)
+            bar= document.createElement('div')
+            bar.classList = "bar"
+            bar.style.height = random_num+'px'
+            barsHeights.push(random_num)
+            barsArray.push(bar)
+            counter++; 
+
+        }
+        
+    }
+
+    function drawBars(barsObjects){
+        let array = barsObjects;
+        console.log(array)
+        const render = array.map((bar)=><Fragment>{bar[0]}</Fragment>)
+        return render;
     }
 
     useEffect(() => {
-       
-        generateBars(5)
+     
+       generateBars(5)
+      
+
     })
 
 
@@ -36,14 +51,14 @@ function SortingViasualizer() {
                         <button>Merge Sort</button>
                     </li>
                     <li>
-                        <button>QuickSort</button>
+                        <button >QuickSort</button>
                     </li>
                 </ul>
             </nav>
 
 
-            <div className="Visualizer flex " id="Visualizer">
-                
+            <div className="Visualizer flex" id="Visualizer">
+            
             </div>
 
 
